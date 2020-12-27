ae56a7f4718b54c0c049112ec3ac2cd9a02591ce
Emmanouil Konstantinidis <hello@manos.im>
2020-12-27 20:35:43 +0000

    Fix layout import

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { WindowLocation } from '@reach/router';
 
-import { Layout } from '../components/layout';
+import { Layout } from '../components/Layout';
 import { Logo } from '../components/Logo';
 import { SEO } from '../components/seo';
 
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { WindowLocation } from '@reach/router';
 
-import { Layout } from '../components/layout';
+import { Layout } from '../components/Layout';
 import { SEO } from '../components/seo';
 
 // prettier-ignore
