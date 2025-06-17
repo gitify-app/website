1d99d1f3926e6d0aa96bb825e37009bac1057ab7
Adam Setch <adam.setch@outlook.com>
2025-06-17 18:05:11 -0400

    feat: update biome import rule
    Signed-off-by: Adam Setch <adam.setch@outlook.com>

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
